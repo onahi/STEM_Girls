9bf6c1ea0aa7db441e51a17fced05117ced33f2b
Paul Ajogi <onahipaul80@gmail.com>
2020-06-23 13:42:01 +0100

    complete navigation

---
+++ script/nav_script.js
@@ -0,0 +1,11 @@
+
+
+
+$(document).ready(function(){
+  $(".nav_icon").click(function(){
+    $(".sidebar").css("display", "block");
+  })
+  $(".close").click(function(){
+    $(".sidebar").css("display", "none");
+  })
+})

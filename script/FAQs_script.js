9706a09218c9b7e671bf0007deddb8498cb7e156
Paul Ajogi <onahipaul80@gmail.com>
2020-08-09 13:59:48 +0100

    Updates and Corrections.

---
+++ script/FAQs_script.js
@@ -0,0 +1,70 @@
+
+
+$(document).ready(function(){
+  $("#q1").click(function(){
+    $("#a1").toggle('slide');
+  })
+})
+
+
+$(document).ready(function(){
+  $("#q2").click(function(){
+    $("#a2").toggle('slide');
+  })
+})
+
+
+$(document).ready(function(){
+  $("#q3").click(function(){
+    $("#a3").toggle('slide');
+  })
+})
+
+
+$(document).ready(function(){
+  $("#q4").click(function(){
+    $("#a4").toggle('slide');
+  })
+})
+
+
+$(document).ready(function(){
+  $("#q5").click(function(){
+    $("#a5").toggle('slide');
+  })
+})
+
+
+$(document).ready(function(){
+  $("#q6").click(function(){
+    $("#a6").toggle('slide');
+  })
+})
+
+
+$(document).ready(function(){
+  $("#q7").click(function(){
+    $("#a7").toggle('slide');
+  })
+})
+
+
+$(document).ready(function(){
+  $("#q8").click(function(){
+    $("#a8").toggle('slide');
+  })
+})
+
+
+$(document).ready(function(){
+  $("#q9").click(function(){
+    $("#a9").toggle('slide');
+  })
+})
+
+
+$(document).ready(function(){
+  $("#q10").click(function(){
+    $("#a10").toggle('slide');
+  })
+})
